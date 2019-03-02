84f1b843cccc408d5e098108e43f048887129c0b
Humberto Pinheiro <humbhenri@gmail.com>
2019-03-01 21:36:54 -0300

    Lights

--- src/main.rs
+++ src/main.rs
@@ -24,46 +24,51 @@ fn scene_intersect<'a>(
     spheres: &[Sphere<'a>],
     hit: &mut Vec3f,
     N: &mut Vec3f,
-    material: &'a mut Material<'a>,
-) -> bool {
+) -> Option<Material<'a>> {
     let mut spheres_dist = std::f32::MAX;
+    let mut material: Material = Default::default();
     for sphere in spheres.iter() {
-        let mut dist_i: &mut f32 = &mut std::f32::MAX;
+        let mut dist_i: &mut f32 = &mut std::f32:: MAX;
         if sphere.ray_intersect(&orig, &dir, &mut dist_i) && *dist_i < spheres_dist {
             spheres_dist = *dist_i;
             *hit = (*orig + *dir) * *dist_i;
             *N = *hit - *sphere.center;
             N.normalize();
-            *material = sphere.material;
+            material = sphere.material;
         }
     }
 
-    spheres_dist < 1000.
+    if spheres_dist < 1000. {
+        Some(material)
+    } else {
+        None
+    }
 }
 
 fn cast_ray(orig: &Vec3f, dir: &Vec3f, spheres: &[Sphere], lights: &[Light]) -> Vec3f {
     let mut point: Vec3f = Vec3f(0., 0., 0.);
     let mut N: Vec3f = Vec3f(0., 0., 0.);
-    let mut material: Material = Default::default();
-    if !scene_intersect(&orig, &dir, &spheres, &mut point, &mut N, &mut material) {
-        return BACKGROUND;
-    };
-    let mut diffuse_light_intensity = 0.;
-    let mut specular_ligth_intensity = 0.;
-    for light in lights.iter() {
-        let mut light_dir = *light.position - point;
-        light_dir.normalize();
-        diffuse_light_intensity += light.intensity * (0.0f32).max(light_dir * N);
-        specular_ligth_intensity += {
-            let a = reflect(&-light_dir, &N) * *dir;
-            let b = (0.0f32).max(-a);
-            let c = b.powf(material.specular_exponent);
-            c * light.intensity
+    match scene_intersect(&orig, &dir, &spheres, &mut point, &mut N) {
+        None => BACKGROUND,
+        Some(material) => {
+            let mut diffuse_light_intensity = 0.;
+            let mut specular_ligth_intensity = 0.;
+            for light in lights.iter() {
+                let mut light_dir = *light.position - point;
+                light_dir.normalize();
+                diffuse_light_intensity += light.intensity * (0.0f32).max(light_dir * N);
+                specular_ligth_intensity += {
+                    let a = reflect(&-light_dir, &N) * *dir;
+                    let b = (0.0f32).max(-a);
+                    let c = b.powf(material.specular_exponent);
+                    c * light.intensity
+                }
+            }
+
+            *material.diffuse_color * diffuse_light_intensity * material.albedo.0 + Vec3f(1., 1., 1.)
+                * specular_ligth_intensity * material.albedo.1
         }
     }
-
-    *material.diffuse_color * diffuse_light_intensity * material.albedo.0 + Vec3f(1., 1., 1.)
-        * specular_ligth_intensity * material.albedo.1
 }
 
 fn render(spheres: &[Sphere], framebuffer: &mut [Vec3f], lights: &[Light]) {
